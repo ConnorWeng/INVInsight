156ac35de5af3bae3fad31ae71c8014e0de13382
zjh <temp2018good@163.com>
2018-03-25 21:58:06 +0800

    初始化代码

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # INVInsight
 INVInsight makes things easy.
-codes
---
+++ server/apps/api/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\api\controller;
+
+class Index
+{
+    public function index()
+    {
+        echo "服务器(thinkphp)连通成功！";
+    }
+
+    public function test()
+    {
+        echo "服务器(thinkphp)！";
+    }
+}
